0b275ea3b09d1dc6d0c7ad16adebe7980356ae3f
Rndraha21 <robinndraha111@gmail.com>
2025-03-20 10:09:23 +0700

    update

--- Inheritance-jenis-data/tugas.py
+++ Inheritance-jenis-data/tugas.py
@@ -18,21 +18,26 @@ class DataAnalyzer(DataProcessor):
         super().__init__(data_tuple, data_list, data_dict)
 
     def average_tuple(self):
-        if all(isinstance)
-        rata_rata = sum(self.data_tuple)/len(self.data_tuple)
-        print(f"Rata-rata data tuple: {rata_rata}")
+        if all(isinstance(x, (int, float)) for x in self.data_tuple):
+            rata_rata = sum(self.data_tuple)/len(self.data_tuple)
+            print(f"Rata-rata data tuple: {rata_rata}")
+        else:
+            print("Rata-rata tidak dapat dijumlahkan")
         
     def max_min(self):
-        maksimum = max(self.data_list)
-        minimum = min(self.data_list)
-        print(f"Nilai list: Maksimum {maksimum} dan {minimum} Minimum")
+        if all(isinstance(x, (int, float)) for x in self.data_list):
+            maksimum = max(self.data_list)
+            minimum = min(self.data_list)
+            print(f"Nilai list: Maksimum {maksimum} dan {minimum} Minimum")
+        else:
+            print("Maksimum dan minimum tidak diketahui")
 
     def display_dict(self):
         print(f"Key data dictionary : {list(self.data_dict.keys())}")
 
 if __name__ == "__main__":
     data_tuple = (30,23,50,84,100)
-    data_list = [30,23,50,84,100]
+    data_list = ["30",23,50,84,100]
     data_dict = {"nama" : "Robin", "umur" : 20, "kab/kota": "Nias"}
     
     analisis = DataAnalyzer(data_tuple, data_list, data_dict)
